4f281200178f026625fb2a3b10f65bc8e9cffd42
AlexanderTL0343 <alextorreslopez3107@gmail.com>
2025-03-30 01:01:15 -0600

    Ingreso de la estructura del paquete

---
+++ Creaciondelpaquetedelproyecto.sql
@@ -0,0 +1,8 @@
+Create or replace PACKAGE FIDE_ProyectoLenguajes_PCK
+as
+End FIDE_ProyectoLenguajes_PCK;
+
+
+Create or replace PACKAGE body FIDE_ProyectoLenguajes_PCK
+as
+end FIDE_ProyectoLenguajes_PCK;
